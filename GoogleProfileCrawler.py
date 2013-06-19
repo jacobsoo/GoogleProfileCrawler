e504a5b8e07c420c7e3958756f23109dea3fe4c2
Jacob Soo <jacobsoo@users.noreply.github.com>
2013-06-19 12:29:05 +0800

    Update and rename GoogleProfileCrawler to GoogleProfileCrawler.py

--- GoogleProfileCrawler.py
+++ GoogleProfileCrawler.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
 
-import urllib
+import urllib,sys
 from bs4 import BeautifulSoup as bs
 
 def main():
     xml = bs(urllib.urlopen('http://www.gstatic.com/s2/sitemaps/profiles-sitemap.xml').read())
-	for i in xml.findAll('loc'):
-		szURL = i.text
-		szLocalFile = i.text[35:]
-	    try:
-	        urllib.urlretrieve(szURL, szLocalFile)
-	        print("Downloaded %s" % szLocalFile)
-	    except Exception, err:
-	        print("%s could not be retrieved" % szURL)
-	print("All done")
+    for i in xml.findAll('loc'):
+        szURL = i.text
+        szLocalFile = i.text[35:]
+        try:
+            urllib.urlretrieve(szURL, szLocalFile)
+            print("Downloaded %s" % szLocalFile)
+        except Exception, err:
+            print("%s could not be retrieved" % szURL)
+    print("All done")
 
 # Banner
 def Banner():
@@ -23,9 +23,9 @@ def Banner():
 
 # Usage
 def help_menu(cmd):
-    print("Usage: %s\n") % (cmd)
+    print("Usage: ./%s\n") % (cmd)
 
 if __name__ == "__main__":
-	Banner()
-	help_menu()
+    Banner()
+    help_menu(sys.argv[0])
     main()
